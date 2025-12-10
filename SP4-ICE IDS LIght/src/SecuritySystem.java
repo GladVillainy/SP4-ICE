1b1ef0c26207a5061aa26fb91b2200f769d8f69e
GladVillainy <lucasbuades00@gmail.com>
2025-12-10 13:04:36 +0100

    worked on the mehtods and add method addLogEntry

--- SP4-ICE IDS LIght/src/SecuritySystem.java
+++ SP4-ICE IDS LIght/src/SecuritySystem.java
@@ -1,11 +1,27 @@
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SecuritySystem {
-    private List<LogEntry> logEntries;
-    private List<Threat> threats;
+   private ArrayList<LogEntry> logEntries = new ArrayList<>();
+   private  ArrayList<Threat> threats;
 
     Rules RS = new Rules();
+
+
+    public void addLogEntry(String name, LocalDateTime timestamp){
+        LogEntry LE = new LogEntry(name, timestamp);
+        logEntries.add(LE);
+    }
+
+    public ArrayList<LogEntry> getLogEntries() {
+        return logEntries;
+    }
+
+    public ArrayList<Threat> getThreats() {
+        return threats;
+    }
 
     public boolean bruteForce(int attempts) {
         int limit = 3;
@@ -18,21 +34,24 @@ public class SecuritySystem {
     }
 
     public boolean excessiveDeletion() {
-        int limit = 15;
+     /*   final int limit = 15;
         if (limit < filesDeleted) {
-            RS.ex
+            RS.excessiveDeletionExecute();
             return true;
         } else {
             return false;
         }
+
+
+      */
+
         return true;
     }
 
-    public boolean offHoursLogin() {
-        LogEntry logEntry = new LogEntry();
+    public boolean offHoursLogin(LocalDateTime timestamp) {
         LocalTime earliestHour = LocalTime.parse("08:00");
         LocalTime latestHour = LocalTime.parse("16:00");
-        if (logEntry.getTimestamp().toLocalTime().isBefore(earliestHour) || logEntry.getTimestamp().toLocalTime.isAfter(latestHour)) {
+        if (timestamp.toLocalTime().isBefore(earliestHour) || timestamp.toLocalTime().isAfter(latestHour)) {
             return true;
         } else {
             return false;
