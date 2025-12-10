11a238e60dd97448be2e6be19d08ca73b49d40ba
GladVillainy <lucasbuades00@gmail.com>
2025-12-10 13:05:34 +0100

    Updated

--- SP4-ICE IDS LIght/src/LogEntry.java
+++ SP4-ICE IDS LIght/src/LogEntry.java
@@ -4,6 +4,15 @@ public class LogEntry {
     private LocalDateTime timestamp;
     private String user;
 
+    public LogEntry(String user, LocalDateTime timestamp) {
+        this.user = user;
+        this.timestamp = timestamp;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
     public LocalDateTime getTimestamp() {
         return timestamp;
     }
