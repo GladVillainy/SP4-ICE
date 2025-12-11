665ffb2c98a0eff1154591a0dc8b8d1d2ecb4378
GladVillainy <lucasbuades00@gmail.com>
2025-12-11 21:47:53 +0100

    Worked on Rules class and updated.

--- SP4-ICE IDS LIght/src/Rules.java
+++ SP4-ICE IDS LIght/src/Rules.java
@@ -1,14 +1,22 @@
 public class Rules {
+    ArrayList<User> lockedUsers = new ArrayList();
+    ArrayList<User> lockedOffHourUsers = new ArrayList();
 
-    public void bruteForceExecute(){
 
+
+    public void bruteForceExecute(User user){
+        user.setLocked(true);
+        lockedUsers.add(user);
+        System.out.println("This user:"+ user.getUsername()+ " is locked");
     }
 
     public void excessiveDeletionExecute(){
 
     }
-    public void offHoursLoginExecute(){
-
+    public void offHoursLoginExecute(User user){
+        user.setLocked(true);
+        lockedOffHourUsers.add(user);
+        System.out.println("This user:"+ user.getUsername()+ " is on off hour lock");
     }
 
 }
